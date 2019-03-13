5e0668f6ab72712210561999b4ae7d180e54076a
Leslie Shea <leslieyshea@gmail.com>
2019-03-13 16:09:32 -0700

    created makeJokesTemplate with passing test

---
+++ src/jokes-component.js
@@ -0,0 +1,11 @@
+export function makeJokesTemplate(joke) {
+    const html = /*html*/
+    `<li>
+        <span class="favorite-heart">♥</span>
+        <p id="${joke.id}">${joke.value}</p>
+    </li>`;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
---
+++ test/make-jokes-list-template.test.js
@@ -0,0 +1,23 @@
+import { makeJokesTemplate } from '../src/jokes-component.js';
+
+const test = QUnit.test;
+QUnit.module('making template for list of jokes');
+
+test('make jokes list template', assert => {
+    // arrange
+    const joke = {
+        id: 'atfmenvtsaatnvplw_lpla',
+        value: 'For undercover police work, Chuck Norris pins his badge underneath his shirt, directly into his chest.'
+    };
+
+    // act
+    const result = makeJokesTemplate(joke);
+    const expected = /*html*/
+    `<li>
+        <span class="favorite-heart">♥</span>
+        <p id="atfmenvtsaatnvplw_lpla">For undercover police work, Chuck Norris pins his badge underneath his shirt, directly into his chest.</p>
+    </li>`;
+
+    // assert
+    assert.htmlEqual(result, expected);
+});
