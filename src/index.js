90a6715b6b4a544136bc3fdf962e97558e00d99b
Leslie Shea <leslieyshea@gmail.com>
2019-03-13 16:59:59 -0700

    tested fetch to make sure we grabbed data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import jokes from '../data/sample-data.js';
+// import jokes from '../data/sample-data.js';
 import loadJokes from './jokes-component.js';
 
-loadJokes(jokes);
+
+const query = encodeURIComponent('pain');
+const url = `https://api.chucknorris.io/jokes/search?query=${query}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(response => {
+        loadJokes(response);
+
+    });
