9abb3b7164dcc2f5348a2f82eccaad7088c48f87
emily baier <emilyrbaier@gmail.com>
2019-03-13 16:52:38 -0700

    added paging and search form input

--- tests/make-jokes-template.test.js
+++ tests/make-jokes-template.test.js
@@ -9,12 +9,9 @@ QUnit.module('template literal html tests');
 test('template matches literal html', assert => {
     //arrange
     const joke = {
-        result: [
-            {
-                id: 'atfmenvtsaatnvplw_lpla',
-                value: 'For undercover police work, Chuck Norris pins his badge underneath his shirt, directly into his chest.'   
-            }
-        ]};
+        id: 'atfmenvtsaatnvplw_lpla',
+        value: 'For undercover police work, Chuck Norris pins his badge underneath his shirt, directly into his chest.'   
+    };
     //act
     const result = makeJokesTemplate(joke);
     const expected = /*html*/ `
