7f9c87d886f75efb3a13e170bebc55f26200895b
Leslie Shea <leslieyshea@gmail.com>
2019-03-14 13:43:48 -0700

    took out searchParams page

--- src/make-url.js
+++ src/make-url.js
@@ -2,7 +2,6 @@ const SEARCH_JOKES_URL = 'https://api.chucknorris.io/jokes/search';
 
 export default function makeUrl(queryOptions) {
     const query = queryOptions.query;
-    const page = queryOptions.page;
 
     if(!query) {
         return '';
@@ -10,7 +9,6 @@ export default function makeUrl(queryOptions) {
 
     const url = new URL(SEARCH_JOKES_URL);
     url.searchParams.set('query', query);
-    url.searchParams.set('page', page);
     
     return url.toString();
 }
