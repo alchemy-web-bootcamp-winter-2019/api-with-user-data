47d817143c18090bbf9f400ee084d59b25177504
Leslie Shea <leslieyshea@gmail.com>
2019-03-14 11:00:16 -0700

    created paging function using array.slice

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './search-component.js';
 import { updateQueryInput } from './search-component.js';
 import { readFromQuery } from './query-component.js';
 import makeUrl from './make-url.js';
+import loadPaging from './paging-component.js';
+import { pageArray } from './paging-component.js';
 
 window.addEventListener('hashchange', () => {
     const query = window.location.hash.slice(1);
@@ -10,10 +12,14 @@ window.addEventListener('hashchange', () => {
     updateQueryInput(queryOptions.query);
 
     const url = makeUrl(queryOptions);
+
     fetch(url)
         .then(response => response.json())
         .then(jokes => {
-            loadJokes(jokes);
+            loadPaging(jokes.result.length, pagingOptions => {
+                const pagedJokes = pageArray(jokes.result, pagingOptions);
+                loadJokes(pagedJokes);
+            });
         })
         .catch(err => {
             /* eslint-disable-next-line */
