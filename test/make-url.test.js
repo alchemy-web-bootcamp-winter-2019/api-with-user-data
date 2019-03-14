16f6f41096cfd5e24e7febe903761eafc14705f2
Leslie Shea <leslieyshea@gmail.com>
2019-03-14 09:53:06 -0700

    moved make-url function to src

--- test/make-url.test.js
+++ test/make-url.test.js
@@ -1,22 +1,7 @@
+import makeUrl from '../src/make-url.js';
+
 const test = QUnit.test;
 QUnit.module('make search url');
-
-const SEARCH_JOKES_URL = 'https://api.chucknorris.io/jokes/search';
-
-function makeUrl(queryOptions) {
-    const query = queryOptions.query;
-    const page = queryOptions.page;
-
-    if(!query) {
-        return '';
-    }
-
-    const url = new URL(SEARCH_JOKES_URL);
-    url.searchParams.set('query', query);
-    url.searchParams.set('page', page);
-    
-    return url.toString();
-}
 
 test('make url with query and page number', assert => {
     const queryOptions = {
