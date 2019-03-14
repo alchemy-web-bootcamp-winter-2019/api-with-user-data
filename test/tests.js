0870649b238c98d6ffd621411c5b72b47f2b49f7
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-14 16:52:44 -0700

    convert object to array test passed

--- test/tests.js
+++ test/tests.js
@@ -4,3 +4,4 @@ import './html-equal.js';
 import './make-starship-template.test.js';
 import './make-search-url.test.js';
 import './query-components.test.js';
+import './convert-object-to-array.test.js';
