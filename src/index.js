18914c14a0ff4cdea645d94b4e0c5941814f412a
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-14 14:05:05 -0700

    deleted notes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import loadStarships from './starships/starship-components.js';
+import loadStarships, { updateStarships } from './starships/starship-components.js';
 import { writeQuery, readQuery } from './starships/query-component.js';
 import './starships/search-component.js';
 import { updateSearchInput } from './starships/search-component.js';
@@ -7,15 +7,6 @@ import { makeSearchUrl } from './make-search-url.js';
 const starshipListNode = document.getElementById('starship-list');
 
 let queryOptions = null;
-
-// loadSearch(newSearchOptions => {
-//    searchOptions = newSearchOptions;
-//    const queryOptions = {
-//        search: searchOptions
-//     };
-//     const query = writeQuery(queryOptions);
-//     window.location.hash = query;
-// });
 
 window.addEventListener('hashchange', loadQuery);
 
@@ -26,11 +17,10 @@ function loadQuery() {
     const queryOptions = readQuery(query);
     updateSearchInput(queryOptions.searchInput);
     const url = makeSearchUrl(queryOptions);
-    console.log(url);
     fetch(url)
         .then(response => response.json())
         .then(starships => {
-            loadStarships(starships.results);
+            updateStarships(starships.results);
         })
         .catch(err => {
             /* eslint-disable-next-line*/
