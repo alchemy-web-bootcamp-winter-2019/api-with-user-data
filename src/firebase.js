9db4e06055998f6eb873c897a666f3d8162d9ad7
Leslie Shea <leslieyshea@gmail.com>
2019-03-14 15:35:35 -0700

    added projectId to config

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,13 @@
 const config = {
     apiKey: 'AIzaSyCklL2pU3fZ1ln6t9TMvKPm2hIfQKoAMek',
     authDomain: 'chuck-norris-jokes-8b27e.firebaseapp.com',
-    databaseURL: 'https://chuck-norris-jokes-8b27e.firebaseio.com'
+    databaseURL: 'https://chuck-norris-jokes-8b27e.firebaseio.com',
+    projectId: 'chuck-norris-jokes-8b27e'
 };
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+const db = firebase.database();
+export const usersRef = db.ref('users');
+export const favoritesByUserRef = db.ref('favorites-by-user');
