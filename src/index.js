62d47727c17c06e65066e2a2360db80bd5cd7d0a
emily baier <emilyrbaier@gmail.com>
2019-03-13 17:50:53 -0700

    added event listener to the window

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-//import jokes from '../data/sample-data.js';
 import loadJokes from './jokes-component.js';
 import './search-component.js';
+import { updateQueryInput } from './search-component.js';
+import { readFromQuery } from './query-component.js';
 
+
+window.addEventListener('hashchange', () => {
+    const query = window.location.hash.slice(1);
+    const queryOptions = readFromQuery(query);
+    updateQueryInput(queryOptions.query);
+});
 
 const query = encodeURIComponent('pain');
 const url = `https://api.chucknorris.io/jokes/search?query=${query}`;
