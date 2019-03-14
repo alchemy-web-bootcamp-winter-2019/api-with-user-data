35c015e59b22b1a9ef4d56cd22bc794140492eb5
Leslie Shea <leslieyshea@gmail.com>
2019-03-14 09:53:18 -0700

    called make-url in fetch and added error

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import loadJokes from './jokes-component.js';
 import './search-component.js';
 import { updateQueryInput } from './search-component.js';
 import { readFromQuery } from './query-component.js';
+import makeUrl from './make-url.js';
 
 window.addEventListener('hashchange', () => {
     const query = window.location.hash.slice(1);
     const queryOptions = readFromQuery(query);
     updateQueryInput(queryOptions.query);
 
+    const url = makeUrl(queryOptions);
+    fetch(url)
+        .then(response => response.json())
+        .then(jokes => {
+            loadJokes(jokes);
+        })
+        .catch(err => {
+            /* eslint-disable-next-line */
+            console.error('Fetch error:', err);
+        });
 });
-
-
-const query = encodeURIComponent('pain');
-const url = `https://api.chucknorris.io/jokes/search?query=${query}`;
-
-fetch(url)
-    .then(response => response.json())
-    .then(response => {
-        loadJokes(response);
-
-    });
