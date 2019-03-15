753ef3d9f688c69b5af439fb9511fbafa737bd63
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-15 11:09:53 -0700

    wrote makeUserProfile function/passing test

--- test/header-component.test.js
+++ test/header-component.test.js
@@ -13,3 +13,43 @@ test('makes header template', assert => {
         </section>`;
     assert.htmlEqual(result, expected);
 })
+function makeUserProfile(user) {
+    let link = null;
+    let linkText = null;
+    if(window.location.pathname === '/favorites.html') {
+        link = '/';
+        linkText = 'Back to Starship Inventory';
+    }
+    else {
+        link = '/favorites.html';
+        linkText = 'My Selected Starships';
+    }
+    const html = /*html*/`
+    <section id="profile">
+            <p>Name: ${user.displayName} </p>
+            <img src="${user.photoURL}">
+            <button>Sign Out</button>
+            <a href="favorites.html">link</a>
+        </section>`;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+test('make user profile', assert => {
+    const user = {
+        displayName: 'Bob Smith',
+        photoURL: 'http://www.nacdnet.org/wp-content/uploads/2016/06/person-placeholder.jpg'
+    };
+    const expected = /*html*/`
+    <section id="profile">
+            <p>Name: Bob Smith </p>
+            <img src="http://www.nacdnet.org/wp-content/uploads/2016/06/person-placeholder.jpg">
+            <button>Sign Out</button>
+            <a href="favorites.html">link</a>
+        </section>`;
+    const result = makeUserProfile(user);
+
+    assert.htmlEqual(result, expected);
+
+})
